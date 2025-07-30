7f9a551c08fc8025a32857483203394ce18f5d98
Eduardo <ecemo10101@gmail.com>
2025-07-31 00:18:51 +0200

    Test the new changes build

--- elements-build.js
+++ elements-build.js
@@ -4,9 +4,8 @@ const concat = require('concat');
 
 (async function build() {
   const files = [
-    './dist/web-component/runtime.js',
-    './dist/web-component/polyfills.js',
-    './dist/web-component/main.js'
+    './dist/web-components/browser/polyfills.js',
+    './dist/web-components/browser/main.js'
   ];
 
   await fs.ensureDir('elements'); // Carpeta de salida final
